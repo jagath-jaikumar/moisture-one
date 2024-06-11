70a74b23fff7f98a0c70edac1859762f7f8eb480
Jagath Jai Kumar <jagath.jaikumar@gmail.com>
2024-06-11 18:53:04 -0400

    speed insights

--- interfaces/moisture-one-web/src/App.js
+++ interfaces/moisture-one-web/src/App.js
@@ -1,10 +1,12 @@
 import "./App.css";
 
+import { SpeedInsights } from "@vercel/speed-insights/next";
 import logo from "./logo.svg";
 
 function App() {
   return (
     <div className="App">
+      <SpeedInsights />
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
